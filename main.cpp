b91d871f22922cb2424b9d8959fd20c76f577148
“Nicholas <nicholas.robishaw1@gmail.com>
2024-07-09 10:46:36 -0700

    Added Subproblem B

--- main.cpp
+++ main.cpp
@@ -19,6 +19,15 @@ class Queries_HT{
 
     int collision_Count;
 
+    char* genome_Data;
+    long genome_Size;
+    long allocated_Genome_Size;
+    unsigned int fragments_Found;
+
+    char** fragments;
+    
+    long scaffold_Count = 0;
+
 
     // file reader for query file
     // read query dataset file function
@@ -44,7 +53,6 @@ class Queries_HT{
                 // attempt to add fragment to hash table
                 // if false is returned then there was a problem and program will clean up memory and end
                 hash_Constructor(current_Line);
-                
             }
         }
 
@@ -57,7 +65,134 @@ class Queries_HT{
 
 
     // file reader for genome file
+    bool file_reader(const string& file_Name) {
+        // variables
+        ifstream file(file_Name);
+        string current_Scaffold_Name;
+        string current_Line;
+        string temp_Genome = "";
+        long prev_genome_size;
+        unsigned long index;
+        time_t stop_Watch = 0;
+
+        // check if the file can be opened
+        if (!file.is_open()) {
+            // display error and exit
+            cerr << "Error: Unable to open file " << file_Name << "\n";
+            return false;
+        }
     
+        // initialize starting array, set to 3 billion to save on runtime complexity
+        allocated_Genome_Size = 3000000000;
+        genome_Data = new char[allocated_Genome_Size];
+        
+        // iterate through file contents line by line
+        while ( scaffold_Count < MAX_SCAFFOLD_COUNT && getline(file, current_Line)) {
+            // check if we hit a header
+            if(current_Line[0] == '>' && !temp_Genome.empty()){
+                
+                // increment scaffold count by 1
+                scaffold_Count++;
+                
+                // check and set timestamps
+                // if(scaffold_Count == 100 || 
+                //     scaffold_Count == 300 || scaffold_Count == 500){
+                        
+                //     time(&stop_Watch);
+                //     cout << "  Read in first " << scaffold_Count << " scaffolds at: " << ctime(&stop_Watch) << endl; 
+                
+                // }
+                
+                // calculate new size for genome array
+                prev_genome_size = genome_Size;
+            
+                // resize the genome array to fit last scaffold and copy data into new array
+                genome_Constructor(temp_Genome, prev_genome_size);
+
+                // reset the temp genome string to empty
+                temp_Genome = "";
+            }
+
+            else if (!current_Line.empty() && current_Line[0] != '>') {
+                // Iterate through current line and append characters to temp_Genome
+                for (index = 0; index < current_Line.length() && index < 80; index++) {
+                    if(                  current_Line[index] == 'A' 
+                                      || current_Line[index] == 'C' 
+                                      || current_Line[index] == 'G' 
+                                      || current_Line[index] == 'T' 
+                                      || current_Line[index] == 'N'){
+                    
+                    
+                        // Append character to temp_Genome
+                        temp_Genome += current_Line[index];
+                    }
+                }
+            }
+        }
+
+        // post append information of last scaffold
+        if (!temp_Genome.empty()) {
+            // calculate new genome array size
+            prev_genome_size = genome_Size;
+            
+            // resize the genome array to fit last scaffold and copy data into new array
+            genome_Constructor(temp_Genome, prev_genome_size);
+        }
+
+        // close the file
+        file.close();
+
+        // return sucess
+        return true;
+        
+    }// end of file reader function
+
+
+    // deconstructor for deallocating the genome character array
+    void genome_Deconstructor(char* genome_arr){
+        delete[] genome_arr;
+        genome_arr=nullptr;
+    }
+
+
+    // function to resize the genome character array
+    void genome_Constructor(const string to_Add, long cat_Index) {
+        long index;
+        long length = to_Add.length();
+        
+        // check if the array needs to be resized
+        if(genome_Size + length >= allocated_Genome_Size){
+            // calculate the new size of the genome
+            long new_Size = genome_Size + length;
+            
+            // allocate new array size
+            char* new_Genome_Arr = new char[new_Size];
+            
+            // copy data from old array to new one
+            for(index = 0; index < genome_Size; index++){
+                new_Genome_Arr[index] = genome_Data[index];
+            }
+            
+            // deallocate memory from old array
+            delete[] genome_Data;
+            
+            // set genome pointer to new array
+            genome_Data = new_Genome_Arr;
+            
+            // set the new allocated size for genome array
+            allocated_Genome_Size = new_Size;
+        }
+        
+            
+        for(index = 0; index < length; index++){
+            //add in the new data 
+            genome_Data[cat_Index + index] = to_Add[index];
+        }
+        
+        // set the new genome size
+        genome_Size = cat_Index + to_Add.length();
+    }
+
 
     // initialize hash table
     void initialize_Hash(){
@@ -66,6 +201,8 @@ class Queries_HT{
         hash_Table = new fragment_Node*[hash_Size];
 
         collision_Count = 0;
+
+        fragments_Found = 0;
 
         // iterate through the hash table and set each index to NULL
         for(int index=0; index < hash_Size; index++){
@@ -210,11 +347,133 @@ class Queries_HT{
         return index;
     }
 
+
+    // iterate through the genome array and attempt to locate every 32 character fragment in the hash table
+    void search_Function(){
+        long index;
+        int high_Index = 0, inner_Index, genome_Index;
+        char test_Frag[fragment_Size-1];
+
+        // loop through the genome array
+        for(index=0; index < genome_Size; index++){
+
+            // create fragment ( index to index+31 )
+            high_Index = index + (fragment_Size - 2);
+
+            genome_Index = index;
+
+            // read into character test frag array
+            for( inner_Index = 0; inner_Index < high_Index; inner_Index++){
+                test_Frag[inner_Index] = genome_Data[genome_Index];
+                genome_Index++;
+            }
+
+            // pass to helper function to look through the hash table ( return type unknown right now )
+            if( lookup_Hash(test_Frag) ){
+                // increment found frag counter
+                fragments_Found++;
+
+                // if fragment was found add to fragment found list ( Possible 2d array of character fragments)
+                found_Frags( test_Frag);
+            }
+
+        }   
+
+    }
+
+
+    // helper function for search_Function to look for fragment in hash table
+    bool lookup_Hash( char test_Fragment[] ){
+        fragment_Node* temp_Ptr = nullptr;
+
+        // convert string to radix notation
+        unsigned int fragment_Radix = radix_Noation(test_Fragment);
+
+        // get the hash index
+        int test_Hash = fragment_Radix % hash_mod;
+
+        // set the temp pointer to the hash index
+        temp_Ptr = hash_Table[test_Hash];
+
+        // loop thorugh the Linked list until hitting null or match
+        while( temp_Ptr != nullptr ){
+            // if the current nodes fragment matches the test fragment
+            // if( compare_Fragments(test_Fragment, temp_Ptr->fragment)){
+            if( strcmp(test_Fragment, temp_Ptr->fragment) == 0){
+                // return success
+                return true;
+            }
+
+            // otherwise move to next node
+            temp_Ptr = temp_Ptr->next_Node;
+        }
+        
+        // otherwise return failure
+        return false;
+    }
+
+    // this function is not being used rn. It can potentially replace strcmp in the lookup_Hash function
+    bool compare_Fragments(string target, char fragment[]){
+        int index;
+        
+        // iterate over the test fragment
+        for( index = 0; index < fragment_Size-1; index++){
+            
+            // check if there is a differnce
+            if(target[index] != fragment[index]){
+                // return failure if difference
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+
+    // function for adding new found fragment to storage
+    void found_Frags( char fragment[]){
+        unsigned int index;
+
+        // make a newly resized array to house the extra found fragment
+        char **new_Arr = new char*[fragments_Found];
+
+        // copy existing pointers from old array to the new resized array
+        for( index = 0; index < fragments_Found - 1; index++){
+            new_Arr[index] = fragments[index];
+        }
+
+        // delete old fragment array
+        delete[] fragments;
+
+        // create a character array for the new sport
+        new_Arr[fragments_Found-1] = new char[fragment_Size];
+
+        // copy new fragment to the new spot
+        for( index = 0; index < fragment_Size; index++){
+            new_Arr[fragments_Found-1][index] = fragment[index];
+        }
+
+    }
+
+
+    void found_Frags_Deconstructor(){
+        unsigned int index;
+
+        // loop through the array of pointers
+        for( index = 0; index < fragments_Found; index++){
+            // free each index
+            delete[] fragments[index];
+        }
+
+        // free main array
+        delete[] fragments;
+    }
 };
 
 int main(int argc, char* argv[]){
     Queries_HT my_Hash;
     time_t stop_Watch = 0;
+    int index, inner_Index;
 
     // check if the program has the appropriate number of parameter
     // create error handler for no input file argument
@@ -252,12 +511,51 @@ int main(int argc, char* argv[]){
     
     cerr << "Finished populating hash table\n";
 
+
+
+    // Subproblem B -  Searching Speed
+    // set hash table to size 60 million
+    // populate the hash table with the sequence fragments from the query dataset.
+    // Read in genome sequence
+    if(my_Hash.file_reader(argv[1])){
+        
+        cout << "\nGenome size: " << my_Hash.genome_Size << endl;
+        
+        time(&stop_Watch);
+        cout << "Starting Search at: " << ctime(&stop_Watch) << endl;
+
+        // implement a search function which would search for 32-character fragments of the subject sequence within the Queries_HT object.
+        // Iterate through all 32-character long fragments of the subject dataset, searching for each one in the query dataset.
+        my_Hash.search_Function();
+            
+        // How long did it take to search for every possible 32 character long fragment of the subject dataset within the query dataset?
+        time(&stop_Watch);
+        cout << "Finished Search at: " << ctime(&stop_Watch) << endl;   
+
+        // How many such fragments did you find
+        cout << "There were " << my_Hash.fragments_Found << " fragments found\n";
+
+        // Print the first 10 fragments of the subject dataset that you found within the Query_HT
+        for( index = 0; index < 10; index++){
+            cout << " ";
+
+            for( inner_Index = 0; inner_Index < my_Hash.fragment_Size; inner_Index++){
+                cout << my_Hash.fragments[index][inner_Index];
+            }
+
+            cout << endl;
+        }
+
+        cout << endl;
+    }
+
+
     
     // display info
     cout << "Hash Size           : " << my_Hash.hash_Size << endl;
     cout << "Number of collisions: " << my_Hash.collision_Count << endl;
     
-    for(int index = 0; index < 10; index++){
+    for(index = 0; index < 10; index++){
         if( my_Hash.hash_Table[index] != nullptr ){
             cout << " ";
             
@@ -274,20 +572,11 @@ int main(int argc, char* argv[]){
     }
     
     
-    // Subproblem B -  Searching Speed
-    // set hash table to size 60 million
-    // populate the hash table with the sequence fragments from the query dataset.
-    // Read in genome sequence
-
-    // implement a search function which would search for 16-character fragments of the subject sequence within the Queries_HT object.
-    // Iterate through all 16-character long fragments of the subject dataset, searching for each one in the query dataset.
-
-        // How long did it take to search for every possible 16 character long fragment of the subject dataset within the query dataset?
-        // How many such fragments did you find
-        // Print the first 10 fragments of the subject dataset that you found within the Query_HT
         
     // clean up hash table
     my_Hash.hash_Deconstructor();
+    my_Hash.genome_Deconstructor(my_Hash.genome_Data);
+    my_Hash.found_Frags_Deconstructor();
 
     cout << "Program End\n";
     return 0;
--- main.h
+++ main.h
@@ -16,7 +16,7 @@ typedef struct fragment_Node{
 
 }fragment_Node;
 
-
+// phase 1
 bool read_Qurey(const string& file_Name);
 void initialize_Hash();
 void hash_Constructor(string fragment_String);
@@ -25,5 +25,15 @@ void hash_Deconstructor();
 void free_LL( fragment_Node* current_Ptr);
 unsigned int radix_Noation(string fragment_Str);
 int character_Value(char letter);
+
+// phase 2
+bool file_reader(const string& file_Name);
+void genome_Deconstructor(char* genome_arr);
+void genome_Constructor(const string to_Add, long cat_Index);
+void search_Function();
+bool lookup_Hash( char test_Fragment[] );
+bool compare_Fragments(string target, char fragment[]);
+void found_Frags( char fragment[]);
+void found_Frags_Deconstructor();
 
 #endif 
